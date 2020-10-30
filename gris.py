6293bfe545c5bda060b64bd755f466373a272cc2
DiegoHori <dhori@LAPTOP-7V4ED2VH.localdomain>
2020-10-29 18:31:19 -0600

    gris

--- gris.py
+++ gris.py
@@ -5,9 +5,10 @@ imagen=cv2.imread("imagen.jpg")
 imagenA=imagen.shape[0]
 imagenH=imagen.shape[1]
 matriz=np.zeros((imagenA,imagenH),np.uint8)
+img2=np.int16(imagen)
 for i in range(imagenA):
     for j in range(imagenH):
-        matriz[i,j]=(imagen[i,j,0]+imagen[i,j,1]+imagen[i,j,2])/3
+        matriz[i,j]=(img2[i,j,0]+img2[i,j,1]+img2[i,j,2])/3
 
 
 cv2.imwrite("imagengris.jpg",matriz)

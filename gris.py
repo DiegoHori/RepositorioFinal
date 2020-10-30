b00326f4fe9c006039cf0944746e74d215dc881d
DiegoHori <dhori@LAPTOP-7V4ED2VH.localdomain>
2020-10-29 20:44:52 -0600

    gris

--- gris.py
+++ gris.py
@@ -10,4 +10,4 @@ def escala_grises(imagen):
             matriz[i,j]=(imagen[i,j,2] + imagen[i,j,1] + imagen[i,j,0])/3
     cv2.imwrite("imagengris.jpg",matriz)
 escala_grises("imagen.jpg")
-####
+#####

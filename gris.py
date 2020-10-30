59ab2ab5c3f452a296db7efce0b59ce6da33483f
Jasiel3118 <73612632+Jasiel3118@users.noreply.github.com>
2020-10-29 19:23:32 -0600

    Update gris.py

--- gris.py
+++ gris.py
@@ -1,14 +1,19 @@
 import cv2
 import numpy as np
-
-imagen=cv2.imread("imagen.jpg")
-imagenA=imagen.shape[0]
-imagenH=imagen.shape[1]
-matriz=np.zeros((imagenA,imagenH),np.uint8)
-img2=np.int16(imagen)
-for i in range(imagenA):
-    for j in range(imagenH):
-        matriz[i,j]=(img2[i,j,0] + img2[i,j,1] + img2[i,j,2])/3
+def escala_grises(imagen):
 
 
-cv2.imwrite("imagengris.jpg",matriz)
+        imagen=cv2.imread("imagen.jpg")
+        imagenh=imagen.shape[0]
+        imagenw=imagen.shape[1]
+        Matriz=np.zeros((imagenh,imagenw),np.uint8)
+        im2=np.int16(imagen)
+        for i in range(imagenh):
+                for j in range(imagenw):
+                        Matriz[i,j]=(im2[i,j,2]+im2[i,j,1]+im2[i,j,0])/3
+
+        cv2.imshow("imagen",imagen)
+        cv2.imshow("imagegn",Matriz)
+        cv2.waitKey(0)
+
+escala_grises("imagen.jpg")
